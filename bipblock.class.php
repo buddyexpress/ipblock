5c7d26152edf58a663063fcd7a63260cd3357d4b
Leonard Walter <arsalan@buddyexpress.net>
2013-08-16 21:07:52 +0500

    Update bipblock.class.php

--- bipblock.class.php
+++ bipblock.class.php
@@ -1,1 +1,11 @@
-<?php
+<?php 
+/**
+ * Buddyexpress Ip Block
+ *
+ * @package   Bframework
+ * @subpackage class.ipblock
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
